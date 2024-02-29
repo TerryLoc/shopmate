c9b44193beb5a5ae7a0172d622129d941e55b388
terryloughran <terryloughran@gmail.com>
2024-02-29 15:59:49 +0000

    Set up

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -2,7 +2,6 @@ import { cloneElement, useEffect, useState } from 'react';
 
 export function ProductList() {
   const [products, setProducts] = useState([]);
-  console.log(products);
 
   useEffect(() => {
     fetch('http://localhost:8000/products')
@@ -11,8 +10,19 @@ export function ProductList() {
   }, []);
 
   return (
-    <div>
-      <p>productlist</p>
-    </div>
+    <section>
+      {products.map((product) => (
+        <div className="card" key={product.id}>
+          <p className="id">{product.id}</p>
+          <p className="name">{product.name}</p>
+          <p className="info">
+            <span>€{product.price}</span>
+            <span className={product.in_stock ? 'instock' : 'unavailable'}>
+              {product.in_stock ? 'In Stock' : 'Unavailable'}
+            </span>
+          </p>
+        </div>
+      ))}
+    </section>
   );
 }

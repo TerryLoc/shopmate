25b0ce2d3fef5210174ba094d2afa0667ec404a4
terryloughran <terryloughran@gmail.com>
2024-02-29 14:33:29 +0000

    useEffect

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,13 +1,14 @@
-import { cloneElement, useState } from 'react';
+import { cloneElement, useEffect, useState } from 'react';
 
 export function ProductList() {
   const [products, setProducts] = useState([]);
-
   console.log(products);
 
-  fetch('http://localhost:8000/products')
-    .then((response) => response.json())
-    .then((data) => console.log(data));
+  useEffect(() => {
+    fetch('http://localhost:8000/products')
+      .then((response) => response.json())
+      .then((data) => setProducts(data));
+  }, []);
 
   return (
     <div>
